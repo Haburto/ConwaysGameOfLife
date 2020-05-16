be3c59c6936f7d63ec7ccde06d02a1d8ecef12d0
Haburto <shadowibsack@live.de>
2020-05-16 17:17:12 +0200

    implemented redraw() and draw_grid()

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pygame
 
-# TODO: create background with pygame
-# TODO: create grid with pygame
 # TODO: create class Cell and implement the logic (rules)
 
 # RULES for Conways Game of Life
@@ -15,6 +13,25 @@ import pygame
 # TODO: test the wikipedia example patterns
 
 
+# TODO: create grid with pygame
+def draw_grid(window, screen_size, rows, columns):
+    size_between_rows = screen_size[0] // rows
+    size_between_columns = screen_size[1] // columns
+    color_lines = (255, 255, 255)
+
+    for row in range(1, rows + 1):
+        pygame.draw.line(window,
+                         color_lines,
+                         (0, row * size_between_rows),
+                         (screen_size[0], row * size_between_rows))
+
+    for column in range(1, columns + 1):
+        pygame.draw.line(window,
+                         color_lines,
+                         (column * size_between_columns, 0),
+                         (column * size_between_columns, screen_size[1]))
+
+
 def event_handler():
     global running
     for event in pygame.event.get():
@@ -23,14 +40,26 @@ def event_handler():
             break
 
 
+def redraw_window(window, screen_size, rows, columns):
+    color_window = (0, 0, 0)
+    window.fill(color_window)
+    draw_grid(window, screen_size, rows, columns)
+    pygame.display.update()
+
+
+# TODO: implement a easy way to change the screen settings
+# like row and column count, colors, etc.
 def main():
     global running
     pygame.init()
 
-    width = 1000
+    width = 800
     height = width
+    screen_size = (width, height)
+    rows = 50
+    columns = rows
 
-    window = pygame.display.set_mode((width, height))
+    window = pygame.display.set_mode(screen_size)
 
     running = True
     while running:
@@ -38,6 +67,7 @@ def main():
         # Else the OS will think that the game has crashed
         # You should also implement the QUIT event first, so that you can comfortably quit the project
         event_handler()
+        redraw_window(window, screen_size, rows, columns)
 
     pygame.quit()
     exit()
